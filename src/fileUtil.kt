7986e69c690a5f1ae7acebe48fdb4236021e4378
hinst <hinstance@yandex.ru>
2015-08-25 18:48:48 +0300

    +getFileDirectory

--- src/fileUtil.kt
+++ src/fileUtil.kt
@@ -1,15 +1,26 @@
 package hinst.HDelphiCrawler
 
+import java.io.File
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 
 val userDirKey = "user.dir"
 
-fun readFile(filePath: String, encoding: Charset): String
-{
+fun readFile(filePath: String, encoding: Charset): String {
 	val bytes = Files.readAllBytes(Paths.get(filePath));
 	return String(bytes, encoding);
 }
 
 fun compareFilePath(aFilePath: String, bFilePath: String) = aFilePath.equals(bFilePath, ignoreCase = checkIfOsIsWindows())
+
+fun getFileDirectory(aFilePath: String): String {
+	while (true) {
+		val index = aFilePath.indexOf(File.separator)
+		if (index >= 0)
+			aFilePath.substring(beginIndex = index + 1)
+		else
+			break
+	}
+	return aFilePath
+}
