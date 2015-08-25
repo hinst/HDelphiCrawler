dc59cb39a17a2fa68e743a7a3d3d1c74120e4e0f
hinst <hinstance@yandex.ru>
2015-08-25 18:31:11 +0300

    fileUtil updated, now has compareFilePath function

--- src/fileUtil.kt
+++ src/fileUtil.kt
@@ -4,8 +4,12 @@ import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
 
+val userDirKey = "user.dir"
+
 fun readFile(filePath: String, encoding: Charset): String
 {
 	val bytes = Files.readAllBytes(Paths.get(filePath));
 	return String(bytes, encoding);
 }
+
+fun compareFilePath(aFilePath: String, bFilePath: String) = aFilePath.equals(bFilePath, ignoreCase = checkIfOsIsWindows())
